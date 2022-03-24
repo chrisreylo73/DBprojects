dd58c8faa9ecb82aaf74cb12a5126c6a29d1bc89
Juan Ruiz <jruiz50@msudenver.edu>
2022-03-23 20:08:41 -0600

    cms java update

--- cms.java
+++ cms.java
@@ -1,1 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class cms {
+
+    public static void main(String[] args) throws Exception {
+        String server = "localhost";
+        String database = "hr";
+        String user = "hr_admin";
+
+        // you should NEVER hard-code passwords like shown in this example in a production environment!!!
+        String password = "135791";
+        String connectURL = "jdbc:postgresql://" + server + "/" + database + "?user=" + user + "&password=" + password;
+
+        // connects to the database
+        Connection conn = DriverManager.getConnection(connectURL);
+        System.out.println("Connection to Postgres database " + database + " was successful!");
+
+        // closes the connection
+        System.out.println("Bye!");
+        conn.close();
+    }
+}
