5fc6a8a04247ab3f11fa30755f340f917acd3a0b
christian lopez <chrisreylo73@gmail.com>
2022-03-24 16:04:54 -0600

    Rough Draft of cms.sql and basic skelecton of cms.java

--- cms.sql
+++ cms.sql
@@ -1,23 +1,57 @@
 -- Team Members: Juan Ruiz, Chris Lopez
 
 -- TODOd: create the cms database
-CREATE DATABASE cms;
+    CREATE DATABASE cms;
 
 -- TODOd: "open" the database for use
-\c cms
+    \c cms
 
 -- TODO: (optional) drop all tables
-
+    DROP TABLE IF EXISTS Providers, Drg, Providers_DiagnosisRelatedGroups, ProviderLocation;
 -- TODO: create all tables (with primary keys, NULL constraints, and foreign keys)
-
+    CREATE TABLE Providers(
+        Rndrng_CCN CHAR(6) PRIMARY KEY,
+        Rndrng_Prvdr_Org_Name VARCHAR(1000) NOT NULL,
+        Rndrng_Prvdr_St VARCHAR(1000) NOt NULL,
+        Rndrng_Prvdr_RUCA VARCHAR(1000) NOT NULL
+    );
+    CREATE TABLE Drg(
+        DRG_Cd CHAR(3) PRIMARY KEY,
+        DRG_Desc VARCHAR(5000) NOT NULL
+    );
+    --(COME BACK TO THIS NOT SURE ABOUT DECIMAL DATATYPE)
+    CREATE TABLE Providers_Drgs(
+        Rndrng_CCN CHAR(6), 
+        DRG_Cd CHAR(3), 
+        Tot_Dschrgs INT NOT NULL, 
+        Avg_Submtd_Cvrd_Chrg DECIMAL NOT NULL,
+        Avg_Tot_Pymt_Amt DECIMAL NOT NULL, 
+        Avg_Mdcr_Pymt_Amt DECIMAL NOT NULL,
+        PRIMARY KEY (Rndrng_CCN, DRG_Cd)
+    );
+    CREATE TABLE ProviderLocations(
+        Rndrng_Prvdr_St VARCHAR(1000) PRIMARY KEY,  
+        Rndrng_Prvdr_City VARCHAR(1000) NOT NULL,  
+        Rndrng_Prvdr_State_Abrvtn CHAR(2) NOT NULL, 
+        Rndrng_Prvdr_State_FIPS CHAR (2) NOT NULL, 
+        Rndrng_Prvdr_Zip5 VARCHAR(1000) NOT NULL
+    );
+    CREATE TABLE Ruca( 
+        Rndrng_Prvdr_RUCA CHAR(1) PRIMARY KEY, 
+        Rndrng_Prvdr_RUCA_Name VARCHAR(1000) NOT NULL, 
+        Rndrng_Prvdr_RUCA_Desc VARCHAR(5000) NOT NULL
+    );
 -- TODO: create users 
-
+    CREATE USER "cms_admin" PASSWORD '024680';
+    CREATE USER "cms" PASSWORD '135791';
 -- TODO: grant access to users 
-
+-- Not correct but this is the idea (come back to this)
+    GRANT ALL ON TABLE Employees TO "cms_admin";
+    GRANT SELECT ON TABLE Employees TO "cms";
 -- TODO: answer all queries
 
 -- a) List all diagnostic names in alphabetical order.   
-  
+
 -- b) List the names and correspondent states (including Washington D.C.) of all of the providers in alphabetical order (state first, provider name next, no repetition).   
   
 -- c) List the total number of providers.  
